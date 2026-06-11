6e2104c7f31548575efa6e1d5a5757bcf63403b7
Bibis🦕​ <gabriella.silva.santos16@escola.pr.gov.br>
2026-06-11 12:26:36 -0300

    Update gabriella.js

--- gabriella.js
+++ gabriella.js
@@ -1,1 +1,22 @@
 
+const btnSaibaMais = document.getElementById("saibaMais");
+
+btnSaibaMais.addEventListener("click", () => {
+    document.getElementById("sobre").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+const btnImpacto = document.querySelector(".btn-info");
+const mensagem = document.getElementById("mensagem");
+
+btnImpacto.addEventListener("click", () => {
+    mensagem.innerHTML =
+        "🌱 A agricultura sustentável ajuda a conservar o solo, reduzir o desperdício de água, proteger a biodiversidade e garantir a produção de alimentos para as futuras gerações.";
+});
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alert("Mensagem enviada com sucesso!");
+});
